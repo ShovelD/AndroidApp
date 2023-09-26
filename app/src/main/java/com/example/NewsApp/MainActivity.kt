da6e79a58757645504062fe04531ae7394800555
ShovelD <gimnazist.ds@gmail.com>
2023-09-26 10:02:26 +0300

    Fixed text alignment
    Text alignmed in the center of row now

--- app/src/main/java/com/example/NewsApp/MainActivity.kt
+++ app/src/main/java/com/example/NewsApp/MainActivity.kt
@@ -31,9 +31,12 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Modifier
 import androidx.compose.material3.Icon
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
 
 import com.example.NewsApp.ui.theme.AppTheme
 
@@ -100,11 +103,17 @@ class MainActivity : ComponentActivity() {
                         .size(200.dp)
                 )
             }
-            repeat(10) {
+            repeat(4) {
                 Row(
-                    modifier = Modifier.padding(2.dp)
+                    modifier = Modifier.padding(5.dp),
+                    horizontalArrangement = Arrangement.Center
                 ) {
-                    Text(text = stringResource(id = R.string.developer_name))
+                    Text(text = stringResource(id = R.string.developer_name),
+                        fontSize = 24.sp,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.align(Alignment.CenterVertically)
+                            .fillMaxWidth()
+                    )
                 }
             }
         }
