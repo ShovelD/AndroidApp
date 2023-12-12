13f72d3249d5a6f762da59aacea8d4f4a6a7d2a2
ShovelD <gimnazist.ds@gmail.com>
2023-12-12 15:06:09 +0300

    made visual

--- app/src/main/java/com/example/newsApp/screens/OverviewScreen.kt
+++ app/src/main/java/com/example/newsApp/screens/OverviewScreen.kt
@@ -1,18 +1,44 @@
 package com.example.newsApp.screens
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.MailOutline
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.integerArrayResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.navigation.NavController
 import com.example.newsApp.MVIFramework.container
 import com.example.newsApp.MVIFramework.subscribe
+import com.example.newsApp.MainActivity
+import com.example.newsApp.R
+import com.example.newsApp.viewmodels.HomeState
 import com.example.newsApp.viewmodels.NewsArticle
 import com.example.newsApp.viewmodels.OverviewEvent
 import com.example.newsApp.viewmodels.OverviewIntent
@@ -38,6 +64,7 @@ fun Overview(navController: NavController,id: UUID?,mode:OverviewMode){
     }
     OverViewComposable(state = state, intent = viewModel::intent)
 }
+
 @Composable
 fun OverViewComposable(state:OverviewState,intent:(OverviewIntent)->Unit){
     when(state){
@@ -46,15 +73,14 @@ fun OverViewComposable(state:OverviewState,intent:(OverviewIntent)->Unit){
         }
         is OverviewState.DisplayingRemoteArticle ->{
             val article = state.article?: NewsArticle.getNotFoundArticle()
-            OverViewContent(article = article, buttonText = "remote"){
+            OverViewContent(article = article, buttonText = "Add to favorite"){
                 intent(OverviewIntent.AddToFavoritesClicked(article))
                 intent(OverviewIntent.ExitClicked)
             }
-
         }
         is OverviewState.DisplayingPrivateArticle ->{
             val article = state.article?: NewsArticle.getNotFoundArticle()
-            OverViewContent(article = article,buttonText = "local"){
+            OverViewContent(article = article,buttonText = "RemoveFromFavorite"){
                 intent(OverviewIntent.RemoveFromFavoritesClicked(article.id))
                 intent(OverviewIntent.ExitClicked)
             }
@@ -65,12 +91,33 @@ fun OverViewComposable(state:OverviewState,intent:(OverviewIntent)->Unit){
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun OverViewContent(article:NewsArticle,buttonText:String,event: ()->Unit){
-    Text(text = article.articleTitle,
-        modifier = Modifier.clickable { event() })
-    Button(onClick = { event() }) {
-        Text(text = buttonText)
+    Scaffold (
+    ){
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+        ) {
+            Text(text = "Title: ${article.articleTitle}", modifier = Modifier.padding(10.dp)
+                .background(MaterialTheme.colorScheme.primary))
+            Text(text = "Author: ${article.articleAuthor}",modifier = Modifier.padding(10.dp)
+                .background(MaterialTheme.colorScheme.primary))
+            Text(text = "Description: ${article.articleDescription}",modifier = Modifier.padding(10.dp)
+                .background(MaterialTheme.colorScheme.primary))
+            Text(text = "Date: ${article.articlePublishingDate}",modifier = Modifier.padding(10.dp)
+                .background(MaterialTheme.colorScheme.primary))
+            Button(
+                onClick = { event() },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.primary)
+            ) {
+                Text(text = buttonText, color = MaterialTheme.colorScheme.secondary)
+            }
+        }
     }
 }
 @Composable
